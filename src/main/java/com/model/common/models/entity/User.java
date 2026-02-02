71ba16e9eb42647fa5104d50657ff40b149b5bb9
sandeep <jsandeepreddy1@gmail.com>
2026-02-02 08:31:11 -0500

    initial commit

---
+++ src/main/java/com/model/common/models/entity/User.java
@@ -0,0 +1,30 @@
+
+package com.model.common.models.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+}
